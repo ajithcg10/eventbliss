46091af6d5ac9a7221d0d0304d51efff4c005bd1
ajith.woodenclouds <ajith.woodenclouds@gmail.com>
2024-01-30 03:04:00 +0530

    route change

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -53,7 +53,7 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
-
+ 
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
@@ -79,20 +79,29 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: 'OK', user: newUser })
   }
 
-  if(eventType === 'user.deleted'){
-    const {id } = evt.data;
-   
-    const deleted = await deleteUser(id!)
-    // if(newUser){
-    //     await clerkClient.users.updateUserMetadata(id,{
-    //         publicMetadata:{
-    //             userId:deleted.id,
-    //         }
-    //     })
-    // }
-    return NextResponse.json({message:"success",user:deleted})
-  } 
+  if (eventType === 'user.updated') {
+    const {id, image_url, first_name, last_name, username } = evt.data
 
+    const user = {
+      firstName: first_name,
+      lastName: last_name,
+      username: username!,
+      photo: image_url,
+    }
+
+    const updatedUser = await updateUser(id, user)
+
+    return NextResponse.json({ message: 'OK', user: updatedUser })
+  }
+
+  if (eventType === 'user.deleted') {
+    const { id } = evt.data
+
+    const deletedUser = await deleteUser(id!)
+
+    return NextResponse.json({ message: 'OK', user: deletedUser })
+  }
+ 
   return new Response('', { status: 200 })
 }
  
