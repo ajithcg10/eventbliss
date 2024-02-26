9178a1cfa3908af7f205539ad13bd4fe6e44406e
ajith.woodenclouds <ajith.woodenclouds@gmail.com>
2024-02-27 00:11:06 +0530

    change router

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,52 +1,40 @@
-import { createOrder } from '@lib/actions/order.action';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse } from 'next/server';
-import { Stripe } from 'stripe';
+import stripe from 'stripe'
+import { NextResponse } from 'next/server'
+import { createOrder } from '@lib/actions/order.action'
 
-const stripe = new Stripe('sk_test_...', {
-   // Ensure to use the latest API version
-});
 
-const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+export async function POST(request: Request) {
+  const body = await request.text()
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    res.status(405).end(); // Method Not Allowed
-    return;
-  }
+  const sig = request.headers.get('stripe-signature') as string
+  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
 
-  const sig = req.headers['stripe-signature'] as string;
-
-  let event: Stripe.Event;
+  let event
 
   try {
-    event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
-  } catch (err:any) {
-    res.status(400).send(`Webhook Error: ${err.message}`);
-    return;
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
+  } catch (err) {
+    return NextResponse.json({ message: 'Webhook error', error: err })
   }
 
-  // Handle the event
-  switch (event.type) {
-    case 'checkout.session.completed':
-      const checkoutSessionCompleted = event.data.object as Stripe.Checkout.Session;
-      const {id,metadata,amount_total} = event.data.object
-      const order = {
-        stripeId:id,
-        eventId:metadata?.eventId || "",
-        buyerId:metadata?.buyerId || "",
-        totalAmount:amount_total ? (amount_total / 100).toString() : '0',
-        createdAt: new Date()
-      }
-      const newOrder = await await createOrder(order)
-      return NextResponse.json({ message: 'OK', order: newOrder })
-      // Then define and call a function to handle the event checkout.session.completed
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
+  // Get the ID and type
+  const eventType = event.type
+
+  // CREATE
+  if (eventType === 'checkout.session.completed') {
+    const { id, amount_total, metadata } = event.data.object
+
+    const order = {
+      stripeId: id,
+      eventId: metadata?.eventId || '',
+      buyerId: metadata?.buyerId || '',
+      totalAmount: amount_total ? (amount_total / 100).toString() : '0',
+      createdAt: new Date(),
+    }
+
+    const newOrder = await createOrder(order)
+    return NextResponse.json({ message: 'OK', order: newOrder })
   }
 
-  // Return a 200 response to acknowledge receipt of the event
   return new Response('', { status: 200 })
 }
